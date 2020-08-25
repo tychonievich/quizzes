d177bfec3b70803b5b72047368434001913f735f
Luther Tychonievich <tychonievich@gmail.com>
2020-08-25 14:53:40 -0400

    remove old approach

--- q2json.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "tools.php";
-array_shift($argv);
-$dump = array();
-foreach($argv as $fn) {
-    $dump[$fn] = qparse($fn, TRUE);
-}
-echo json_encode($dump, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
-?>
